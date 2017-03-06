2e19be91523316723f647feb04a39fb609f2b52a
Nicholas Baltodano <nbalto2567@students.pcci.edu>
2017-03-05 21:46:25 -0600

    New project Sandwhich

---
+++ SandwhichOrdering/SandwhichOrdering/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SandwhichOrdering
+{
+    enum  sandwhichType { BLT, potroast, turkey, ham};
+    class Order
+    {
+    }
+}
