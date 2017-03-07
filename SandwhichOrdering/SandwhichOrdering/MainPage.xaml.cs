23e44884add2459876719a63232f992213c9276a
Nicholas Baltodano <nbalto2567@students.pcci.edu>
2017-03-07 15:46:20 -0600

    Added fry and soda buttons

--- SandwhichOrdering/SandwhichOrdering/MainPage.xaml.cs
+++ SandwhichOrdering/SandwhichOrdering/MainPage.xaml.cs
@@ -13,18 +13,186 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
+
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace SandwhichOrdering
+namespace SandwichOrdering
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+    
     public sealed partial class MainPage : Page
     {
+        private Order customerOrder;
+
+        public struct Sandwich
+        {
+            private condiments wKetchup,
+                               wMustard,
+                               wOnions,
+                               wRelish,
+                               wMayonnaise,
+                               wMushrooms;
+            private sandwichType type;
+
+            #region get and sets for variables
+            public condiments WKetchup
+            {
+                get
+                {
+                    return wKetchup;
+                }
+                set
+                {
+                    if (value == condiments.ketchup || value == Constants.NONE)
+                        wKetchup = value;
+                    else
+                        throw new ArgumentException();
+                }  
+            }
+            public condiments WMustard
+            {
+                get
+                {
+                    return wMustard;
+                }
+                set
+                {
+                    if (value == condiments.mustard || value == Constants.NONE)
+                        wMustard = value;
+                    else
+                        throw new ArgumentException();
+                }
+            }
+            public condiments WOnions
+            {
+                get
+                {
+                    return wOnions;
+                }
+                set
+                {
+                    if (value == condiments.onions || value == Constants.NONE)
+                        wOnions = value;
+                    else
+                        throw new ArgumentException();
+                }
+            }
+            public condiments WRelish
+            {
+                get
+                {
+                    return wRelish;
+                }
+                set
+                {
+                    if (value == condiments.relish || value == Constants.NONE)
+                        wRelish = value;
+                    else
+                        throw new ArgumentException();
+                }
+            }
+            public condiments WMayonnaise
+            {
+                get
+                {
+                    return wMayonnaise;
+                }
+                set
+                {
+                    if (value == condiments.mayonnaise  || value == Constants.NONE)
+                        wMayonnaise = value;
+                    else
+                        throw new ArgumentException();
+                }
+            }
+            public condiments WMushrooms
+            {
+                get
+                {
+                    return wMushrooms;
+                }
+                set
+                {
+                    if (value == condiments.mushroom || value == Constants.NONE)
+                        wMushrooms = value;
+                    else
+                        throw new ArgumentException();
+                }
+            }
+            public sandwichType Type
+            {
+                get
+                {
+                    return type;
+                }
+                set
+                {
+                    type = value;
+                }
+            }
+            #endregion
+
+            public override string ToString()
+            {
+              
+                string sandwichSummary = this.Type.ToString();
+
+                if ((WKetchup != Constants.NONE) || (WMustard != Constants.NONE) || (WOnions != Constants.NONE) ||
+                    (WRelish != Constants.NONE) || (WMayonnaise != Constants.NONE) || (WMayonnaise != Constants.NONE))
+                {
+                    if (WKetchup == condiments.ketchup)
+                        sandwichSummary += "\n with Ketchup";
+                    if (WMustard == condiments.mustard)
+                        sandwichSummary += "\n with Mustard";
+                    if (WOnions == condiments.onions)
+                        sandwichSummary += "\n with Onions";
+                    if (WRelish == condiments.relish)
+                        sandwichSummary += "\n with Relish";
+                    if (WMayonnaise == condiments.mayonnaise)
+                        sandwichSummary += "\n with Mayonnaise";
+                    if (WMushrooms == condiments.mushroom)
+                        sandwichSummary += "\n with Mushrooms";
+                }
+                else
+                {
+                    sandwichSummary += " with no condiments";
+                }
+
+                return sandwichSummary;
+            }
+        }
+
+
+
         public MainPage()
         {
             this.InitializeComponent();
+            customerOrder = new Order();
+            
         }
+
+        private void addSoda_Click(object sender, RoutedEventArgs e)
+        {
+            customerOrder.addDrink();
+            sodaCount.Text = customerOrder.DrinkCount.ToString();
+        }
+
+        private void minusSoda_Click(object sender, RoutedEventArgs e)
+        {
+            customerOrder.removeDrink();
+            sodaCount.Text = customerOrder.DrinkCount.ToString();
+        }
+      
+        private void addFries_Click(object sender, RoutedEventArgs e)
+        {
+            customerOrder.addFry();
+            fryCount.Text = customerOrder.FryCount.ToString();
+        }
+
+        private void minusFries_Click(object sender, RoutedEventArgs e)
+        {
+            customerOrder.removeFry();
+            fryCount.Text = customerOrder.FryCount.ToString();
+        }
+
     }
 }
--- SandwhichOrdering/SandwhichOrdering/Order.cs
+++ SandwhichOrdering/SandwhichOrdering/Order.cs
@@ -3,11 +3,108 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static SandwichOrdering.MainPage;
 
-namespace SandwhichOrdering
+namespace SandwichOrdering
 {
-    enum  sandwhichType { BLT, potroast, turkey, ham};
+    // Enumerations
+    public enum sandwichType { BLT, potroast, turkey, ham};
+    public enum condiments   { ketchup = 1, mustard, onions, relish, mayonnaise, mushroom};
+
+    static class Constants
+    {
+        public const int    MIN_SANDWICHES = 0;
+        public const int    MIN_SODAS      = 0;
+        public const int    MIN_FRIES      = 0;
+        public const int    NONE           = 0;
+        public const double FRY_COST       = 1.25;
+        public const double SODA_COST      = 1.00;
+        public const double SANDWICH_COST  = 5.00;        
+    }
+    
     class Order
     {
+        private List<Sandwich> sandwichList;
+        private static int fryCount;
+        private static int drinkCount;
+
+        #region Property Accessors
+        public int FryCount
+        {
+            get
+            {
+                return fryCount;
+            }
+            set
+            {
+                if (value >= Constants.MIN_FRIES)
+                    fryCount = value;
+                else
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+        public int DrinkCount
+        {
+            get
+            {
+                return drinkCount;
+            }
+            set
+            {
+                if (value >= Constants.MIN_SODAS)
+                    drinkCount = value;
+                else
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+        #endregion
+
+        public Order(int fryCount = 0, int drinkCount = 0)
+        {
+            this.FryCount = fryCount;
+            this.DrinkCount = drinkCount;
+            this.sandwichList = new List<Sandwich>();
+        }
+
+
+        public double totalCart()
+        {
+            Double total = 0.0;
+
+            foreach (Sandwich sandwhich in sandwichList)
+            {
+                total += Constants.SANDWICH_COST;
+            }
+            total += (DrinkCount * Constants.SODA_COST);
+            total += (FryCount * Constants.FRY_COST);
+            return total;
+        }
+
+        public void addDrink()
+        {
+            this.DrinkCount++;
+        }
+
+        public void removeDrink()
+        {
+            if (this.DrinkCount > 0)
+                this.DrinkCount--;
+        }
+
+        public void addFry()
+        {
+            this.FryCount++;
+        }
+
+        public void removeFry()
+        {
+            if (this.FryCount > 0)
+                this.FryCount--;
+        }
+
+        public void addSandwich(Sandwich newSandwhich)
+        {
+            this.sandwichList.Add(newSandwhich);
+        }
     }
 }
